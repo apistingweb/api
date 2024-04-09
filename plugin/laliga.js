7772856bc8324011daaf63ac60113b910d752aa7
apistingweb <143703659+apistingweb@users.noreply.github.com>
2024-04-09 15:48:21 +0200

    Create laliga.js

---
+++ plugin/laliga.js
@@ -0,0 +1,8 @@
+/*==============================
+~> Name Temp     : STING WEB - Plugin Scoure & LaLiga - Auto
+~> Version	 : 2024 / 1.2 MENA
+~> Last Updated  : 10 - 5 - 2024 : 12:00 AM - 02:50 PM
+~> Dev By     	 : Sting Dev  - Facebook Page ->  https://www.facebook.com/stingweb.eg
+~> Desgin URL    : sting-web.blogspot.com
+===============================*/
+a
